1015fd7b8f651347f7846da84cdd7065efc4f247
chonki <abdullahbr98@gmail.com>
2022-06-27 02:09:59 +0500

    final changes

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ import { TransactionsProvider } from './context/TransactionContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <TransactionsProvider>
-        <App />
-    </TransactionsProvider>
     {/* <ChakraProvider theme={theme}> */}
-    <App />
+    <TransactionsProvider>
+      <App />
+    </TransactionsProvider>
     {/* </ChakraProvider> */}
   </React.StrictMode>
 );
